6b49d583031a4c0e45c12a546708522b655b2a65
df275 <df275@EVIL>
2026-05-07 23:54:42 +0500

    Добавлены базовые классы MVVM: Notification и RealyCommand

---
+++ UserRoleManager_Чернышков/Classes/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UserRoleManager_Чернышков.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
