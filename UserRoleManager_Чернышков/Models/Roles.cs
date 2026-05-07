7ec21a6c196c123019dcbc5f9b3d276063f35617
df275 <df275@EVIL>
2026-05-08 00:07:28 +0500

    Добавлены модели Users и Roles. Чернышков

---
+++ UserRoleManager_Чернышков/Models/Roles.cs
@@ -0,0 +1,27 @@
+﻿using UserRoleManager_Чернышков.Classes;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserRoleManager_Чернышков.Models
+{
+    public class Roles : Notification
+    {
+        public int Id { get; set; }
+
+        private string name = string.Empty;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        [Schema.NotMapped]
+        public string RoleInfo
+        {
+            get { return $"Роль: {Name}"; }
+        }
+    }
+}
---
+++ UserRoleManager_Чернышков/Models/Users.cs
@@ -0,0 +1,42 @@
+﻿using UserRoleManager_Чернышков.Classes;
+using Schema = System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserRoleManager_Чернышков.Models
+{
+    public class Users : Notification
+    {
+        public int Id { get; set; }
+
+        private string fio = string.Empty;
+        public string FIO
+        {
+            get { return fio; }
+            set
+            {
+                fio = value;
+                OnPropertyChanged("FIO");
+            }
+        }
+
+        private string login = string.Empty;
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+
+        public int IdRole { get; set; }
+
+        public Roles? Role { get; set; }
+
+        [Schema.NotMapped]
+        public string RoleName
+        {
+            get { return Role != null ? Role.Name : "Без роли"; }
+        }
+    }
+}
