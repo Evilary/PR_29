29c0403404bb249960484c4728e3fb99673c5ea8
df275 <df275@EVIL>
2026-05-08 01:37:38 +0500

    Добавлены страницы Users и Roles. Чернышков

---
+++ UserRoleManager_Чернышков/View/Roles.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace UserRoleManager_Чернышков.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Roles.xaml
+    /// </summary>
+    public partial class Roles : Page
+    {
+        public Roles(object context)
+        {
+            InitializeComponent();
+            DataContext = context;
+        }
+    }
+}
---
+++ UserRoleManager_Чернышков/View/Users.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace UserRoleManager_Чернышков.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Users.xaml
+    /// </summary>
+    public partial class Users : Page
+    {
+        public Users(object context)
+        {
+            InitializeComponent();
+            DataContext = context;
+        }
+    }
+}
+
