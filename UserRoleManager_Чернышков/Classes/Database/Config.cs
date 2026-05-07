48f7c9328856660afa667c839cda97913b11a95b
df275 <df275@EVIL>
2026-05-08 00:40:23 +0500

    Подключены EF Core и MySQL, добавлен контекст данных. Чернышков

---
+++ UserRoleManager_Чернышков/Classes/Database/Config.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace UserRoleManager_Чернышков.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1;uid=root;pwd=;database=UserRoleManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
---
+++ UserRoleManager_Чернышков/Context/UserRoleContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserRoleManager_Чернышков.Classes.Database;
+using UserRoleManager_Чернышков.Models;
+
+namespace UserRoleManager_Чернышков.Context
+{
+    public class UserRoleContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; } = null!;
+        public DbSet<Roles> Roles { get; set; } = null!;
+
+        public UserRoleContext()
+        {
+            Database.EnsureCreated();
+            Set<Users>().Load();
+            Set<Roles>().Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.connection, Config.version);
+    }
+}
